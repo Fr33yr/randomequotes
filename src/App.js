54c8d2e6922403bbaa1107ba5e38d64c35bd0b46
Fr33yr <75025925+Fr33yr@users.noreply.github.com>
2022-03-10 22:58:19 -0300

    prev styles

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export function App() {
         <p id='author'>- AuthorName</p>
         <div className='buttons'>
           <ul>
-            <li><a id='tweet-quote' href='#'>Twitter</a></li>
-            <li><a id='tweet-quote' href='#'>Instagram</a></li>
+            <li><a id='tweet-quote' href='#'><i className="fa-brands fa-twitter-square"></i></a></li>
+            <li><a id='tweet-quote' href='#'><i className="fa-brands fa-instagram-square"></i></a></li>
           </ul>
           <button id='new-quote'>New quote</button>
         </div>
